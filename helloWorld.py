3008310b5b23e8c158d0d8c08a921e8ccb58068a
unknown <limbachiajaneya@gmail.com>
2021-09-06 23:54:10 +0530

    added prompt

--- helloWorld.py
+++ helloWorld.py
@@ -4,5 +4,8 @@ Created on Mon Sep  6 23:39:26 2021
 
 @author: Janeya Limbachia
 """
-
-print("Hello World!")
+s=input("New to the Worl(Y/N)?")
+if s=="Y":
+    print("Hello World!")
+else:
+    print("Welcome back!")
